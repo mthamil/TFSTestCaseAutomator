95e8749ea5ca6b6e5d6b2a73073f06efceb1ef00
Matthew Hamilton <matthamilton@live.com>
2014-08-12 00:08:08 -0500

    Removed unnecessary using statement.

--- TestCaseAutomator/ViewModels/MainViewModel.cs
+++ TestCaseAutomator/ViewModels/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.TeamFoundation;
