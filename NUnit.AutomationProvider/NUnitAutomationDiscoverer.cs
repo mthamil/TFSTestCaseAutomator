bdc48d966d07ade2ea5ff4d3a2ac845bd0a6d7bd
Matthew Hamilton <matthamilton@live.com>
2015-10-15 14:18:47 -0500

    Added base skeleton of NUnit plugin.

---
+++ NUnit.AutomationProvider/NUnitAutomationDiscoverer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using TestCaseAutomator.AutomationProviders.Interfaces;
+
+namespace NUnit.AutomationProvider
+{
+    /// <summary>
+    /// Finds NUnit tests for test case automation.
+    /// </summary>
+    [Export(typeof(ITestAutomationDiscoverer))]
+    public class NUnitAutomationDiscoverer : ITestAutomationDiscoverer
+    {
+        /// <summary>
+        /// Initializes a new <see cref="NUnitAutomationDiscoverer"/>.
+        /// </summary>
+        [ImportingConstructor]
+        public NUnitAutomationDiscoverer()
+        {
+            
+        }
+
+        /// <see cref="ITestAutomationDiscoverer.SupportedFileExtensions"/>
+        public IEnumerable<string> SupportedFileExtensions => Extensions;
+
+        /// <see cref="ITestAutomationDiscoverer.DiscoverAutomatedTestsAsync"/>
+        public Task<IEnumerable<ITestAutomation>> DiscoverAutomatedTestsAsync(IEnumerable<string> sources)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static readonly ICollection<string> Extensions = new HashSet<string> { ".dll", ".exe" };
+    }
+}
