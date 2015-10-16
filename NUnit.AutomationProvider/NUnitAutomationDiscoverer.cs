f517b1bc943390162eede6d1eb066727c897724a
Matthew Hamilton <matthamilton@live.com>
2015-10-15 20:37:06 -0500

    Implemented NUnit test discovery.

--- NUnit.AutomationProvider/NUnitAutomationDiscoverer.cs
+++ NUnit.AutomationProvider/NUnitAutomationDiscoverer.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using NUnit.Common;
+using NUnit.Engine;
+using NUnit.Engine.Internal;
 using TestCaseAutomator.AutomationProviders.Interfaces;
 
 namespace NUnit.AutomationProvider
@@ -11,13 +18,21 @@ namespace NUnit.AutomationProvider
     [Export(typeof(ITestAutomationDiscoverer))]
     public class NUnitAutomationDiscoverer : ITestAutomationDiscoverer
     {
+        private readonly Func<ITestEngine> _engineFactory;
+
         /// <summary>
         /// Initializes a new <see cref="NUnitAutomationDiscoverer"/>.
         /// </summary>
         [ImportingConstructor]
         public NUnitAutomationDiscoverer()
+            : this(() => new TestEngine()) { }
+
+        /// <summary>
+        /// Initializes a new <see cref="NUnitAutomationDiscoverer"/>.
+        /// </summary>
+        public NUnitAutomationDiscoverer(Func<ITestEngine> engineFactory)
         {
-            
+            _engineFactory = engineFactory;
         }
 
         /// <see cref="ITestAutomationDiscoverer.SupportedFileExtensions"/>
@@ -26,7 +41,29 @@ namespace NUnit.AutomationProvider
         /// <see cref="ITestAutomationDiscoverer.DiscoverAutomatedTestsAsync"/>
         public Task<IEnumerable<ITestAutomation>> DiscoverAutomatedTestsAsync(IEnumerable<string> sources)
         {
-            throw new System.NotImplementedException();
+            var package = new TestPackage(sources.Where(IsTestAssembly).ToList());
+            package.AddSetting(PackageSettings.DisposeRunners, true);
+            package.AddSetting(PackageSettings.ShadowCopyFiles, true);
+            package.AddSetting(PackageSettings.ProcessModel, ProcessModel.Single.ToString());
+            package.AddSetting(PackageSettings.DomainUsage, DomainUsage.None.ToString());
+
+            var engine = _engineFactory();
+            using (var runner = engine.GetRunner(package))
+            {
+                var result = runner.Explore(TestFilter.Empty);
+
+                var tests = (
+                    from assemblyNode in result.SelectNodes("//test-suite[@type='Assembly']").Cast<XmlNode>()
+                    from testNode in assemblyNode.SelectNodes("//test-case").Cast<XmlNode>()
+                    select new NUnitTestAutomation(testNode, assemblyNode.Attributes["fullname"].Value)).ToList<ITestAutomation>();
+
+                return Task.FromResult<IEnumerable<ITestAutomation>>(tests);
+            }
+        }
+
+        private static bool IsTestAssembly(string source)
+        {
+            return Extensions.Contains(Path.GetExtension(source));  // Quick check for .NET assembly file extensions.
         }
 
         private static readonly ICollection<string> Extensions = new HashSet<string> { ".dll", ".exe" };
---
+++ NUnit.AutomationProvider/NUnitTestAutomation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using TestCaseAutomator.AutomationProviders.Interfaces;
+
+namespace NUnit.AutomationProvider
+{
+    /// <summary>
+	/// Represents an NUnit automated test.
+	/// </summary>
+    public class NUnitTestAutomation : ITestAutomation
+    {
+        /// <summary>
+	    /// Initializes a new <see cref="NUnitTestAutomation"/>.
+	    /// </summary>
+	    public NUnitTestAutomation(XmlNode testNode, string file)
+        {
+            Name = testNode.Attributes["fullname"].Value;
+            Identifier = IdentifierFactory.CreateIdentifier(Name);
+            Storage = Path.GetFileName(file);
+            TestType = "Unit Test";
+        }
+
+        /// <see cref="ITestAutomation.Identifier"/>
+        public Guid Identifier { get; }
+
+        /// <see cref="ITestAutomation.Name"/>
+        public string Name { get; }
+
+        /// <see cref="ITestAutomation.TestType"/>
+        public string TestType { get; }
+
+        /// <see cref="ITestAutomation.Storage"/>
+        public string Storage { get; }
+
+        private static readonly ITestIdentifierFactory IdentifierFactory = new HashedIdentifierFactory();
+    }
+}
